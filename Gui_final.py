a4bf880d3cac030a8673a518dafbb02ab1cb2336
Kai14-24 <esauhdz18@outlook.com>
2026-07-24 14:29:26 -0500

    Update Gui_final.py

--- Gui_final.py
+++ Gui_final.py
@@ -11,7 +11,7 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
-        """her we have the dog bread and the buttom for it to do the search"""
+        #her we have the dog bread and the buttom for it to do the search
         self.breads = QtWidgets.QPushButton(self.centralwidget)
         self.breads.setGeometry(QtCore.QRect(20, 20, 110, 25))
         self.breads.setObjectName("breads")
@@ -19,7 +19,7 @@ class Ui_MainWindow(object):
         self.button_bread.setGeometry(QtCore.QRect(140, 55, 210, 25))
         self.button_bread.setObjectName("button_bread")
 
-        """here we have the dog weight the actual weight"""
+        #here we have the dog weight the actual weight
         self.label_input_weight = QtWidgets.QLabel(self.centralwidget)
         self.label_input_weight.setGeometry(QtCore.QRect(20, 60, 100, 25))
         self.label_input_weight.setObjectName("label_input_weight")
@@ -28,12 +28,12 @@ class Ui_MainWindow(object):
         self.input_weight.setPlaceholderText("Ej. 24.5")
         self.input_weight.setObjectName("input_weight")
 
-        """here we have the  button for dog recommended weight calculation"""
+        #here we have the  button for dog recommended weight calculation
         self.button_recommend_weight = QtWidgets.QPushButton(self.centralwidget)
         self.button_recommend_weight.setGeometry(QtCore.QRect(140, 95, 210, 30))
         self.button_recommend_weight.setObjectName("button_recommend_weight")
 
-        """here we have the dog weight the actual weight result"""
+        #here we have the dog weight the actual weight result
         self.label_ideal_weight = QtWidgets.QLabel(self.centralwidget)
         self.label_ideal_weight.setGeometry(QtCore.QRect(20, 140, 110, 25))
         self.label_ideal_weight.setObjectName("label_ideal_weight")
@@ -46,15 +46,24 @@ class Ui_MainWindow(object):
         self.label_status = QtWidgets.QLabel(self.centralwidget)
         self.label_status.setGeometry(QtCore.QRect(140, 175, 210, 25))
         self.label_status.setObjectName("label_status")
+
+        #here we have the input and label for the lifespan
         self.label_lifespan = QtWidgets.QLabel(self.centralwidget)
         self.label_lifespan.setGeometry(QtCore.QRect(20, 210, 110, 25))
         self.label_lifespan.setObjectName("label_lifespan")
         self.lifespan = QtWidgets.QLineEdit(self.centralwidget)
         self.lifespan.setGeometry(QtCore.QRect(140, 210, 110, 25))
         self.lifespan.setObjectName("lifespan")
+        #input age
+        self.label_age = QtWidgets.QLabal(self.centralwidget)
+        self.label_age.setGeometry(QtCore.QRect(20, 310, 110, 25))
+        self.label_age.setObjectname("label_age")
+        self.label_age_status = QtWidgets.QLabal(self.centralwidget)
+        self.label_age_status.setGeometry(QtCore.QRect(140, 310, 450, 25))
+        self.label_age_status.setObjectname("label_age_status")
+        
 
-        """here we have the pro and cons of the dog bread"""
-
+        #here we have the pro and cons of the dog bread
         #Pros
         self.label_pros = QtWidgets.QLabel(self.centralwidget)
         self.label_pros.setGeometry(QtCore.QRect(20, 245, 80, 20))
@@ -73,11 +82,21 @@ class Ui_MainWindow(object):
         self.txt_cons.setReadOnly(True)
         self.txt_cons.setObjectName("txt_cons")
 
+        #The error label
+        self.label_error = QtWidgets.QTextxEdit(self.centralwidget)
+        self.label_error.setGeometry(QtCore.QRect(140, 365, 450, 20))
+        self.label_error.setStyleSheet("color: Red;")
+        self.label_error.setObjectName("label_error")
+
+        MainWindow.setCentrelWidget(self.centralwidget)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotaByName(MainWindow)
+
     def retranslateUi(self, MainWindow:QtWidgets.QMainWindow) -> None:
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Calculate ideal Weight"))
         self.breads.setText(_translate("MainWindow", "Breads"))
-        self.label_input_weight.setText(_translate("MainWindow", "Actual Weight"))
+        self.label_input_weight.setText(_translate("MainWindow", "Actual Weight (kg)"))
         self.label_ideal_weight.setText(_translate("MainWindow", "Ideal range"))
         self.label_status_titel.setText(_translate("MainWindow", "Result:"))
         self.label_lifespan.setText(_translate("MainWindow", "Lifespan"))
