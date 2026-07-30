7f78f554d3c8c67a8268ddab03c2172a8196478e
Kai14-24 <esauhdz18@outlook.com>
2026-07-30 13:03:39 -0500

    Add docstring to main function
    Added docstring to the main function for clarity.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from Breed import Logic
 
 
 def main():
+    """Main entrences to the runing the PyQt6 application for and the logic"""
     app = QApplication(sys.argv)
     window = Logic()
     window.show()
